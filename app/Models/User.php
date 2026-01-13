30c34bdd0e46e8e44f37d4f78004e403846446a3
Unity <Unity@rbiu.local>
2026-01-13 09:28:05 +0500

    Создания базы данных

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,13 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'middlename',
+        'lastname',
+        'tel',
+        'login',
     ];
+
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('middlename')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('login')->nullable();
+            $table->string('tel')->nullable();
             $table->string('email')->unique();
+            $table->string('role')->nullable()->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
