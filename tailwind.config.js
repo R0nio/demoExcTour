aca97874d2c8e3341b92e9bb90a478eb983288a9
Sm0ke <dimabirucob@gmail.com>
2026-01-27 10:28:05 +0500

    slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            color: {
+                'gray-100': '#7376e6'
+            },
         },
     },
 
